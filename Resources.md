5a2b3bbf9c78d9cc757c1969cac7692bf3020582
Shane Ducksbury <shane@sd-mba.local>
2022-09-08 12:09:37 +1000

    Upload Functionality working on frontend now.

--- Resources.md
+++ Resources.md
@@ -12,3 +12,9 @@ Below are a list of resources I used to help build out this project.
 [TypeStack Class Transformer](https://github.com/typestack/class-transformer)
 [Multer Pipe to Omtimize Uploaded Images](https://dev.to/andersonjoseph/nestjs-creating-a-pipe-to-optimize-uploaded-images-5b3h)
 [Integrating Minio with NestJS](https://betterprogramming.pub/upload-and-retrieve-images-by-integrating-minio-with-nestjs-419e4e629b5d)
+
+
+React
+[RXJS in React](https://www.youtube.com/watch?v=Urv82SGIu_0)
+[React Dropzone Tutorial](https://www.youtube.com/watch?v=MAw0lQKqjRA)
+[Upload a file with a progress bar](https://dev.to/jbrocher/react-tips-tricks-uploading-a-file-with-a-progress-bar-3m5p)
--- app/src/views/Upload.tsx
+++ app/src/views/Upload.tsx
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { Button } from '@mui/material'
+import axios from 'axios';
 
 type Props = {}
 
-const Upload = (props: Props) => {
+interface PostData {
+  image: File | null;
+}
+
+const Upload: React.FunctionComponent = (props: Props) => {
+
+  const [formValues, setFormValues] =  useState<PostData>({image: null})
+  const [uploadProgress, setUploadProgress] = useState<Number>(0)
+
+  useEffect(() => {
+    if(formValues.image) handleSubmit()
+  },[formValues.image])
+
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    formValues.image && formData.append("file", formValues.image);
+
+    const response = await axios.post("http://localhost:5050/images/upload", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      },
+      onUploadProgress: (progressEvent: ProgressEvent) => {
+        // This is ready for the upload progress thing
+        const progress = (progressEvent.loaded / progressEvent.total) * 50
+        console.log(progress)
+        setUploadProgress(progress)
+      }
+    })
+
+    setFormValues({image: null})
+
+    console.log(response)
+    return response.data
+  }
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues(() => ({
+      image: event.target.files ? event.target.files[0] : null
+    }))
+  }
+
   return (
-    <h1>Upload</h1>
+    <div className='upload-container'>
+      <h1>Upload</h1>
+      <p>{formValues.image?.name ? formValues.image?.name : "Select File to Upload"}</p>
+      <Button component="label">
+        <input onChange={handleImageChange} type="file" hidden />
+        {formValues.image?.name ? "" : "Select"}
+      </Button>
+      <Button onClick={handleSubmit}>
+        Submit
+      </Button>
+    </div>
+
   )
 }
 
